0fa69afb5e7cd9b5522520ec1c1606f718649d2e
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-08-11 21:32:24 +0200

    get model performance

--- adsimpact/__main__.py
+++ adsimpact/__main__.py
@@ -1,4 +1,4 @@
-from adsimpact import dataprep
+from adsimpact import dataprep,modeldef
     
 def main():
 
@@ -6,8 +6,13 @@ def main():
     Prepare data for training: 
     '''
     data = dataprep.ProductData()
-    data.data_prep()
-    print(data.prepdata.head())
+    data.data_train()
+    model = modeldef.get_model(data.x_train,data.y_train,'load')
 
+    y_pred = model.predict(data.x_test)
+    try_pred = model.predict(data.x_train)
+
+    modeldef.do_train(data.y_test,y_pred)
+    modeldef.do_summary(data.x_test,data.y_test,y_pred,data.x_train,data.y_train,try_pred)
 if __name__ == "__main__":
     main()
