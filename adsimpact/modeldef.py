84f95d3b6b107c5ac11a37c659cc8e326b12d785
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-08-11 21:32:11 +0200

    model performance estimates

--- adsimpact/modeldef.py
+++ adsimpact/modeldef.py
@@ -1,6 +1,9 @@
 import pickle
 import os
 import xgboost
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 def load_model():
     try: 
@@ -36,3 +39,24 @@ def build_model():
     model = xgboost.XGBRegressor(**param)
 
     return model
+
+def do_train(y,pred):
+    heatmap, xedges, yedges = np.histogram2d(y, np.abs(pred-y), bins=50)
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    plt.imshow(heatmap.T,  extent=extent, origin='lower')
+    plt.savefig("Plots/abs_diff.png", transparent=True)
+
+def do_summary(X,Y,Y_pred,trX,trY,trY_pred):
+    with open("summary.txt", "w") as f:
+        f.write("Parameters test(train):\n")
+        f.write("MAE = {} ({}) \n".format(mean_absolute_error(Y,Y_pred),mean_absolute_error(trY,trY_pred)))
+        f.write("MSE = {} ({}) \n".format(mean_squared_error(Y,Y_pred),mean_squared_error(trY,trY_pred)))
+        f.write("mean Ytest={} \n".format(Y.mean()))
+        #f.write("act MAE = {}, MSE = {}) \n".format(np.exp(mean_absolute_error(Y,Y_pred)), np.exp(mean_squared_error(Y,Y_pred))))
+        #f.write("act mean Ytest= {}\n".format(np.exp(Y.mean())) )
+
+    print("MAE = {} ({}) ".format(mean_absolute_error(Y,Y_pred),mean_absolute_error(trY,trY_pred)))
+    print("MSE = {} ({}) ".format(mean_squared_error(Y,Y_pred),mean_squared_error(trY,trY_pred)))
+    print("mean Ytest= ",Y.mean())
+    #print("act MAE = {}, MSE = {}) ".format(np.exp(mean_absolute_error(Y,Y_pred)), np.exp(mean_squared_error(Y,Y_pred))))
+    #print("act mean Ytest= ",np.exp(Y.mean()))
