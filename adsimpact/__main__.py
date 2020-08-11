de6961e5eb7358b2312e24abd6f034bafed544a9
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-08-11 20:30:13 +0200

    load two ds and merge using the three columns 'Product', 'Channel', 'WeekStart'

--- adsimpact/__main__.py
+++ adsimpact/__main__.py
@@ -6,6 +6,8 @@ def main():
     Prepare data for training: 
     '''
     data = dataprep.ProductData()
+    data.load_data()
+    print(data.cleandata.head())
 
 if __name__ == "__main__":
     main()
--- adsimpact/dataprep.py
+++ adsimpact/dataprep.py
@@ -8,3 +8,17 @@ class ProductData:
         self.filename1=filename1
         self.filename2=filename2
 
+    def df_clean(self,df):
+        df = df.fillna(0)
+        df = df.drop_duplicates()
+        return df
+    
+    def load_data(self):
+        data1 = pd.read_csv(self.data_dir+self.filename1)
+        data2 = pd.read_csv(self.data_dir+self.filename2)
+        data1 = self.df_clean(data1)
+        data2 = self.df_clean(data2)
+        result = pd.merge(data1, data2, on=['Product', 'Channel','WeekStart'])
+        result = result.sort_values(by=['WeekStart'])
+        self.cleandata = result
+
