7ff2122f185cbab4dfa9029925281bcfe1896899
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-08-11 20:17:54 +0200

    load data class

--- adsimpact/__main__.py
+++ adsimpact/__main__.py
@@ -1,11 +1,11 @@
-
+from adsimpact import dataprep
     
 def main():
 
     '''
-    Prepare data for training:
+    Prepare data for training: 
     '''
-
+    data = dataprep.ProductData()
 
 if __name__ == "__main__":
     main()
