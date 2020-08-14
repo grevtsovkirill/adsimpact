d934b20cdbee9ed82f43e02f34d7914c77fcff6f
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-08-14 11:52:16 +0200

    inverse transform

--- adsimpact/__main__.py
+++ adsimpact/__main__.py
@@ -1,5 +1,7 @@
 from adsimpact import dataprep,modeldef
-    
+
+import numpy as np
+
 def main():
 
     '''
@@ -7,12 +9,14 @@ def main():
     '''
     data = dataprep.ProductData()
     data.data_train()
-    model = modeldef.get_model(data.x_train,data.y_train,'load')
+    
+    model = modeldef.get_model(data.x_train,data.y_train,'baseline') #
 
-    y_pred = model.predict(data.x_test)
-    try_pred = model.predict(data.x_train)
+    y_pred = np.expm1(model.predict(data.x_test))
+    try_pred = np.expm1(model.predict(data.x_train))
 
-    modeldef.do_train(data.y_test,y_pred)
-    modeldef.do_summary(data.x_test,data.y_test,y_pred,data.x_train,data.y_train,try_pred)
+    modeldef.do_perf_map(np.expm1(data.y_test),y_pred)
+    modeldef.do_summary(np.expm1(data.y_test),y_pred,np.expm1(data.y_train),try_pred)
+
 if __name__ == "__main__":
     main()
